ef8eb9c634c745010b41f0429191d04a99171bbf
Ravi6565-android <ravichauhanis420@gmail.com>
2023-06-29 20:20:39 +0530

    delet ok. upadte is not.

--- app/src/main/java/com/example/retrofit_op/home_sreen/Home_screen_actvity.java
+++ app/src/main/java/com/example/retrofit_op/home_sreen/Home_screen_actvity.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import static com.example.retrofit_op.slapsh_screen.Splash_screen_activity.editor;
 import static com.example.retrofit_op.slapsh_screen.Splash_screen_activity.preferences;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
@@ -37,6 +38,7 @@ import com.example.retrofit_op.model_class.ProductAddModel;
 import com.example.retrofit_op.model_class.Product_get_model;
 import com.example.retrofit_op.model_class.Productdatum;
 import com.example.retrofit_op.model_class.Userdata;
+import com.example.retrofit_op.model_class.model_class;
 import com.example.retrofit_op.register_activity.Register_activity;
 import com.example.retrofit_op.retro_instance;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -67,9 +69,10 @@ public class Home_screen_actvity extends AppCompatActivity {
     Button button;
     String imagedata;
     ProgressBar progressBar;
+    List<Productdatum> Userdata;
 
 
-    ImageView pimage, h_img;
+    ImageView pimage;
     int gellery = 10;
 
     @Override
@@ -86,72 +89,74 @@ public class Home_screen_actvity extends AppCompatActivity {
         logout();
 
 
-        floatingActionButton.setOnClickListener(v -> {
-
-
-
-            Dialog dialog = new Dialog(Home_screen_actvity.this);
-            dialog.setContentView(R.layout.add_product_dialog);
-
-            EditText pname, pprice, pdisc;
-
-            int uid;
-            uid = preferences.getInt("userid", 0);
-            Button add, cancel;
-            pname = dialog.findViewById(R.id.apname);
-            pprice = dialog.findViewById(R.id.apprice);
-            pdisc = dialog.findViewById(R.id.apdis);
-            pimage = dialog.findViewById(R.id.apimage);
-            add = dialog.findViewById(R.id.add);
-            cancel = dialog.findViewById(R.id.cancel);
-
-
-            cancel.setOnClickListener(view -> {
-                dialog.dismiss();
-            });
-            pimage.setOnClickListener(view -> {
-
-                CropImage.activity()
-                        .setGuidelines(CropImageView.Guidelines.ON)
-                        .start(this);
-            });
-
-
-            add.setOnClickListener(v1 -> {
-                String spname, spprice, spdisc;
-                spdisc = pdisc.getText().toString();
-                spprice = pprice.getText().toString();
-                spname = pname.getText().toString();
-
-                Bitmap bitmap = ((BitmapDrawable) pimage.getDrawable()).getBitmap();
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-                byte[] imageinarayy = byteArrayOutputStream.toByteArray();
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    imagedata = Base64.getEncoder().encodeToString(imageinarayy);
-                }
-
-                retro_instance.callApi().PRODUCT_ADD_MODEL_CALL(uid, spname, spprice, spdisc, imagedata)
-                        .enqueue(new Callback<ProductAddModel>() {
-                            @Override
-                            public void onResponse(Call<ProductAddModel> call, Response<ProductAddModel> response) {
-                                if (response.body().getProductaddd() == 1) {
-                                    Toast.makeText(Home_screen_actvity.this, "product Added", Toast.LENGTH_LONG).show();
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(Call<ProductAddModel> call, Throwable t) {
-
-                            }
-                        });
-
-
-                dialog.dismiss();
-            });
-
-            dialog.show();
-        });
+//        floatingActionButton.setOnClickListener(v -> {
+//
+//
+//
+//            Dialog dialog = new Dialog(Home_screen_actvity.this);
+//            dialog.setContentView(R.layout.add_product_dialog);
+//
+//            EditText pname, pprice, pdisc;
+//
+//            int uid;
+//            uid = preferences.getInt("userid", 0);
+//            Button add, cancel;
+//            pname = dialog.findViewById(R.id.apname);
+//            pprice = dialog.findViewById(R.id.apprice);
+//            pdisc = dialog.findViewById(R.id.apdis);
+//            pimage = dialog.findViewById(R.id.apimage);
+//            add = dialog.findViewById(R.id.add);
+//            cancel = dialog.findViewById(R.id.cancel);
+//
+//
+//            cancel.setOnClickListener(view -> {
+//                dialog.dismiss();
+//            });
+//            pimage.setOnClickListener(view -> {
+//
+//                CropImage.activity()
+//                        .setGuidelines(CropImageView.Guidelines.ON)
+//                        .start(this);
+//            });
+//
+//
+//            add.setOnClickListener(v1 -> {
+//                String spname, spprice, spdisc;
+//                spdisc = pdisc.getText().toString();
+//                spprice = pprice.getText().toString();
+//                spname = pname.getText().toString();
+//
+//                Bitmap bitmap = ((BitmapDrawable) pimage.getDrawable()).getBitmap();
+//                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+//                byte[] imageinarayy = byteArrayOutputStream.toByteArray();
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                    imagedata = Base64.getEncoder().encodeToString(imageinarayy);
+//                }
+//
+//                retro_instance.callApi().PRODUCT_ADD_MODEL_CALL(uid, spname, spprice, spdisc, imagedata)
+//                        .enqueue(new Callback<ProductAddModel>() {
+//                            @Override
+//                            public void onResponse(Call<ProductAddModel> call, Response<ProductAddModel> response) {
+//                                if (response.body().getProductaddd() == 1) {
+//                                    Toast.makeText(Home_screen_actvity.this, "product Added", Toast.LENGTH_LONG).show();
+//                                    show_product_method();
+//                                }
+//                            }
+//
+//                            @Override
+//                            public void onFailure(Call<ProductAddModel> call, Throwable t) {
+//
+//                            }
+//                        });
+//
+//
+//                dialog.dismiss();
+//            });
+//
+//            dialog.show();
+//        });
+        newmethod();
 
 
     }
@@ -187,16 +192,12 @@ public class Home_screen_actvity extends AppCompatActivity {
         retro_instance.callApi().PRODUCT_GET_MODEL_CALL(userid).enqueue(new Callback<Product_get_model>() {
             @Override
             public void onResponse(Call<Product_get_model> call, Response<Product_get_model> response) {
-                List<Productdatum> Userdata;
+
                 Userdata= response.body().getProductdata();
                 if(Userdata!=null){
                     progressBar.setVisibility(View.GONE);
                 }
-                for (int i = 0; i < Userdata.size(); i++) {
-                    Log.d("API", "onResponse: name="+Userdata.get(i).getPname());
-                }
 
-//
                 if (response.isSuccessful()) {
 
 
@@ -219,6 +220,80 @@ public class Home_screen_actvity extends AppCompatActivity {
         });
 
     }
+
+    public void newmethod(){
+        recyclerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Dialog dialog = new Dialog(Home_screen_actvity.this);
+                dialog.setContentView(R.layout.add_product_dialog);
+
+                EditText pname, pprice, pdisc;
+
+                int uid;
+                uid = preferences.getInt("userid", 0);
+                Button add, cancel;
+                pname = dialog.findViewById(R.id.apname);
+                pprice = dialog.findViewById(R.id.apprice);
+                pdisc = dialog.findViewById(R.id.apdis);
+                pimage = dialog.findViewById(R.id.apimage);
+                add = dialog.findViewById(R.id.add);
+                cancel = dialog.findViewById(R.id.cancel);
+
+
+                cancel.setOnClickListener(view1 -> {
+                    dialog.dismiss();
+                });
+                pimage.setOnClickListener(view1 -> {
+
+                    CropImage.activity()
+                            .setGuidelines(CropImageView.Guidelines.ON)
+                            .start(Home_screen_actvity.this);
+                });
+
+
+                add.setOnClickListener(v1 -> {
+                    String spname, spprice, spdisc;
+                    spdisc = pdisc.getText().toString();
+                    spprice = pprice.getText().toString();
+                    spname = pname.getText().toString();
+
+                    Bitmap bitmap = ((BitmapDrawable) pimage.getDrawable()).getBitmap();
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+                    byte[] imageinarayy = byteArrayOutputStream.toByteArray();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                        imagedata = Base64.getEncoder().encodeToString(imageinarayy);
+                    }
+                    String path=Userdata.get(0).getPimage();
+
+                    retro_instance.callApi().call_update(2,spname,spprice,spdisc,imagedata,path).enqueue(new Callback<model_class>() {
+                        @Override
+                        public void onResponse(Call<model_class> call, Response<model_class> response) {
+                            if (response.isSuccessful()){
+                                if(response.body().getResult()==1){
+                                    Toast.makeText(Home_screen_actvity.this, "updated product", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<model_class> call, Throwable t) {
+
+                        }
+                    });
+
+
+                    dialog.dismiss();
+                });
+
+                dialog.show();
+
+            }
+        });
+    }
+
 
     private void logout() {
 //        imageView.setOnClickListener(view -> {
@@ -260,6 +335,7 @@ public class Home_screen_actvity extends AppCompatActivity {
             }
         });
     }
+
 
 
     @Override
